c31bb18510a68d90c56152db2e75aa11e955761d
Alex Richardson <alexander1.richardson@gmail.com>
2019-09-27 17:28:57 +0100

    add css

---
+++ myScript.js
@@ -0,0 +1,16 @@
+function Person(first, last, age, eye){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColour = eye;
+    this.fullName = function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+var dad = new Person("John","Doe", 50, "Blue");
+var mum = new Person("Sally","Rally", 48, "Green");
+
+function myFunction(){
+    document.getElementById("demo").innerHTML = dad.fullName() + " " + mum.fullName();
+}
